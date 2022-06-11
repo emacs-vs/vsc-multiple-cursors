6f4b7b57189740bd57ae243cbe68c706308ba52c
Jen-Chieh Shen <jcs090218@gmail.com>
2022-06-11 17:35:11 +0800

    update src

--- vsc-multiple-cursors.el
+++ vsc-multiple-cursors.el
@@ -40,7 +40,13 @@
   :group 'tools
   :link '(url-link :tag "Repository" "https://github.com/emacs-vs/vsc-multiple-cursors"))
 
-(defun jcs-mc/mark-previous-like-this-line ()
+(defcustom vsc-multiple-cursors-similarity 20
+  "The standard similarity, the lower require more precision."
+  :type 'number
+  :group 'vsc-multiple-cursors)
+
+;;;###autoload
+(defun vsc-multiple-cursors-mark-previous-like-this-line ()
   "Smart marking previous line."
   (interactive)
   (require 'multiple-cursors)
@@ -50,37 +56,37 @@
       (unless (> before-unmark-cur-cnt (mc/num-cursors))
         (call-interactively #'mc/mark-previous-like-this)))))
 
-(defun jcs-mc/mark-next-like-this-line ()
+;;;###autoload
+(defun vsc-multiple-cursors-mark-next-like-this-line ()
   "Smart marking next line."
   (interactive)
-  (require 'multiple-cursors)
   (let ((before-unmark-cur-cnt (mc/num-cursors))
         (unmark-do (ignore-errors (call-interactively #'mc/unmark-previous-like-this))))
     (unless unmark-do
       (unless (> before-unmark-cur-cnt (mc/num-cursors))
         (call-interactively #'mc/mark-next-like-this)))))
 
-(defun jcs-mc/maybe-multiple-cursors-mode ()
+(defun vsc-multiple-cursors--maybe-multiple-cursors-mode ()
   "Maybe enable `multiple-cursors-mode' depends on the cursor number."
   (if (> (mc/num-cursors) 1) (multiple-cursors-mode 1) (multiple-cursors-mode 0)))
 
-(defun jcs-mc/to-furthest-cursor-before-point ()
+(defun vsc-multiple-cursors--furthest-cursor-before-point ()
   "Goto the furthest cursor before point."
   (when (mc/furthest-cursor-before-point) (goto-char (overlay-end (mc/furthest-cursor-before-point)))))
 
-(defun jcs-mc/to-furthest-cursor-after-point ()
+(defun vsc-multiple-cursors--furthest-cursor-after-point ()
   "Goto furthest cursor after point."
   (when (mc/furthest-cursor-after-point) (goto-char (overlay-end (mc/furthest-cursor-after-point)))))
 
-(defun jcs-mc/mark-previous-similar-this-line (&optional sdl)
+;;;###autoload
+(defun vsc-multiple-cursors-mark-previous-similar-this-line (&optional sdl)
   "Mark previous line similar to this line depends on string distance level (SDL)."
   (interactive)
-  (require 'multiple-cursors)
-  (unless sdl (setq sdl jcs-mc/string-distance-level))
+  (unless sdl (setq sdl vsc-multiple-cursors-similarity))
   (save-excursion
     (let ((cur-line (thing-at-point 'line)) (cur-col (current-column))
           sim-line break)
-      (jcs-mc/to-furthest-cursor-before-point)
+      (vsc-multiple-cursors--furthest-cursor-before-point)
       (forward-line -1)
       (while (and (not break) (not (= (line-number-at-pos (point)) (line-number-at-pos (point-min)))))
         (setq sim-line (thing-at-point 'line))
@@ -92,17 +98,17 @@
           (setq break t))
         (forward-line -1))
       (unless break (user-error "[INFO] no previous similar match"))))
-  (jcs-mc/maybe-multiple-cursors-mode))
+  (vsc-multiple-cursors--maybe-multiple-cursors-mode))
 
-(defun jcs-mc/mark-next-similar-this-line (&optional sdl)
+;;;###autoload
+(defun vsc-multiple-cursors-mark-next-similar-this-line (&optional sdl)
   "Mark next line similar to this line depends on string distance level (SDL)."
   (interactive)
-  (require 'multiple-cursors)
-  (unless sdl (setq sdl jcs-mc/string-distance-level))
+  (unless sdl (setq sdl vsc-multiple-cursors-similarity))
   (save-excursion
     (let ((cur-line (thing-at-point 'line)) (cur-col (current-column))
           sim-line break)
-      (jcs-mc/to-furthest-cursor-after-point)
+      (vsc-multiple-cursors--furthest-cursor-after-point)
       (forward-line 1)
       (while (and (not break) (not (= (line-number-at-pos (point)) (line-number-at-pos (point-max)))))
         (setq sim-line (thing-at-point 'line))
@@ -114,19 +120,21 @@
           (setq break t))
         (forward-line 1))
       (unless break (user-error "[INFO] no next similar match"))))
-  (jcs-mc/maybe-multiple-cursors-mode))
+  (vsc-multiple-cursors--maybe-multiple-cursors-mode))
 
-(defun jcs-mc/inc-string-distance-level ()
+;;;###autoload
+(defun vsc-multiple-cursors-inc-similarity ()
   "Increase the string distance level by 1."
   (interactive)
-  (setq jcs-mc/string-distance-level (1+ jcs-mc/string-distance-level))
-  (message "[INFO] Current string distance: %s" jcs-mc/string-distance-level))
+  (setq vsc-multiple-cursors-similarity (1+ vsc-multiple-cursors-similarity))
+  (message "[INFO] MC similarity: %s" vsc-multiple-cursors-similarity))
 
-(defun jcs-mc/dec-string-distance-level ()
+;;;###autoload
+(defun vsc-multiple-cursors-dec-similarity ()
   "Decrease the string distance level by 1."
   (interactive)
-  (setq jcs-mc/string-distance-level (1- jcs-mc/string-distance-level))
-  (message "[INFO] Current string distance: %s" jcs-mc/string-distance-level))
+  (setq vsc-multiple-cursors-similarity (1- vsc-multiple-cursors-similarity))
+  (message "[INFO] MC similarity: %s" vsc-multiple-cursors-similarity))
 
 (provide 'vsc-multiple-cursors)
 ;;; vsc-multiple-cursors.el ends here
